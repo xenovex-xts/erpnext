c71fc4c2a2deb3cd753b4c0154cb9b250d06fc49
praveen-xts <praveen.r@xenovex.com>
2026-06-22 16:46:57 +0530

    Postgresql Fix

--- erpnext/stock/doctype/price_list/price_list.py
+++ erpnext/stock/doctype/price_list/price_list.py
@@ -53,30 +53,30 @@ class PriceList(Document):
 	# 		(self.currency, cint(self.buying), cint(self.selling), self.name),
 	# 	)
 	def update_item_price(self):
-        if frappe.db.db_type == "postgres":
-                frappe.db.sql(
-                        """
-                        UPDATE "tabItem Price"
-                        SET currency=%s,
-                                buying=%s,
-                                selling=%s,
-                                modified=NOW()
-                        WHERE price_list=%s
-                        """,
-                        (self.currency, cint(self.buying), cint(self.selling), self.name),
-                )
-        else:
-                frappe.db.sql(
-                        """
-                        UPDATE `tabItem Price`
-                        SET currency=%s,
-                                buying=%s,
-                                selling=%s,
-                                modified=NOW()
-                        WHERE price_list=%s
-                        """,
-                        (self.currency, cint(self.buying), cint(self.selling), self.name),
-                )
+		if frappe.db.db_type == "postgres":
+			frappe.db.sql(
+				"""
+				UPDATE "tabItem Price"
+				SET currency=%s,
+					buying=%s,
+					selling=%s,
+					modified=NOW()
+				WHERE price_list=%s
+				""",
+				(self.currency, cint(self.buying), cint(self.selling), self.name),
+			)
+		else:
+			frappe.db.sql(
+				"""
+				UPDATE `tabItem Price`
+				SET currency=%s,
+					buying=%s,
+					selling=%s,
+					modified=NOW()
+				WHERE price_list=%s
+				""",
+				(self.currency, cint(self.buying), cint(self.selling), self.name),
+			)
 
 	def on_trash(self):
 		self.delete_price_list_details_key()
